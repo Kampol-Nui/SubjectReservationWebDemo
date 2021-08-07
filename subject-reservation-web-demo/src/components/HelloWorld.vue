dd646c29d9c61d0b9283450bfcc6873d12689346
Kampol Thaipricha <kampol.43@mail.kmutt.ac.th>
2021-08-07 14:41:54 +0700

    edit title to netlify

--- subject-reservation-web-demo/src/components/HelloWorld.vue
+++ subject-reservation-web-demo/src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify {{msg}} on netify
+          Welcome to Vuetify {{msg}} on netify on npm
         </h1>
 
         <p class="subheading font-weight-regular">
